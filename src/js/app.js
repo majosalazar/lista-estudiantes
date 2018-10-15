2e3e4327bd9f676ec7a7985df71e6f0614d3d719
majosalazar <mariaj.salazarf@gmail.com>
2018-10-15 17:05:49 -0300

    agrega estudiantes y las guarda en la base de datos

--- src/js/app.js
+++ src/js/app.js
@@ -7,47 +7,105 @@ firebase.initializeApp({
 // Initialize Cloud Firestore through Firebase
 var db = firebase.firestore();
 
-//agregar mensajes 
-function enviar () {
-  //guarda texto
-  let texto = document.getElementById('mensaje').value;
-
-  db.collection("mensajes").add({
-    mensaje: texto
+//agregar estudiante 
+function nuevaEstudiante () {
+  //guarda nombre alumna
+  let nombre = document.getElementById('nombre').value;
+  
+  db.collection("estudiantes").add({
+    nombre: nombre, 
+    skills: null
   })
   .then(function(docRef) {
     console.log("Document written with ID: ", docRef.id);
-    //limpia caja de texto
-    document.getElementById('mensaje').value = ' ';
+    //limpiar input nombre
+    document.getElementById('nombre').value = ' ';
   })
   .catch(function(error) {
     console.error("Error adding document: ", error);
   });
 };
 
-//ver mansajes 
-let mostrarMensajes = document.getElementById('posteo');
 
-db.collection("mensajes").onSnapshot((querySnapshot) => {
-  posteo.innerHTML = ' ';
+//ver tarjeta de cada alumna ingresada
+let estudiantes = document.getElementById('mostrarEstudiantes');
+
+db.collection("estudiantes").onSnapshot((querySnapshot) => {
+  mostrarEstudiantes.innerHTML = ' ';
   querySnapshot.forEach((doc) => {
-    console.log(`${doc.id} => ${doc.data().mensaje}`);
-    posteo.innerHTML += `
-    <div>
-      <p>${doc.data().mensaje}</p>
-      <button onclick="eliminar('${doc.id}')">eliminar</button>
+    console.log(`${doc.id} => ${doc.data().nombre}`);
+    mostrarEstudiantes.innerHTML += `
+    <div class="row">
+      <div class="col-2"> 
+        <img src="" alt="">
+      </div>
+      <div class="col-2">
+        <p>${doc.data().nombre}</p>
+      </div>
+      <div class="col-8">
+        <p>Skills</p>
+        <div id="agregarSkills"></div>
+        <a href="#!" data-toggle="modal" data-target="#exampleModal"">+ agregar</a>
+
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1"         role="dialog" aria-labelledby="exampleModalLabel"         aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title"         id="exampleModalLabel">Add new skills</h5>
+                <button type="button" class="close"         data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+                </div>
+                <div class="modal-body">
+                Separate multiple skill with commas.
+                <br>
+                <textarea id="nuevaSkills"></textarea>
+                
+              </div>
+              <div class="modal-footer">
+              <button type="button" class="btn btn-primary" onclick="agregarSkills()">Add</button>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
     `
   });
 });
 
-//borrar mensaje
-function eliminar (id) {
-  db.collection("mensajes").doc(id).delete().then(function() {
-    console.log("Document successfully deleted!");
-  }).catch(function(error) {
-    console.error("Error removing document: ", error);
+function agregarSkills (id,skills) {
+  console.log("hola")
+  let nuevasSkills = document.getElementById('nuevaSkills').value;
+
+  //actualizar datos 
+  var washingtonRef = db.collection("estudiantes").doc(id); 
+
+  // Set the "capital" field of the city 'DC'
+  return washingtonRef.update({
+    skills: nuevasSkills
+  })
+  .then(function() {
+    console.log("Document successfully updated!");
+    //limpia caja de texto
+    document.getElementById('nuevaSkills').value = ' ';
+    
+  })
+  .catch(function(error) {
+    // The document probably doesn't exist.
+    console.error("Error updating document: ", error);
   });
-
+  
+  
 }
+/*
+//mostrar nuevas skills
+agregarSkills.innerHTML += `
+<button type="button" class="btn btn-primary btn-sm">${doc.data().nuevasSkills}</button>
 
+
+`
+
+*/
